258d7ea081b7a9bc180952bcdc88ccbcd5cccf7a
Shashank Kumar Pandey <iamskp99@gmail.com>
2021-07-30 04:40:53 +0530

    Update

--- main.py
+++ main.py
@@ -124,6 +124,7 @@ def to_huffman_encoded(encoded_data,encode_map):
     for i in encoded_data:
         arr.append(encode_map[i])
 
+    # print(arr)
     return "".join(arr)
 
 
@@ -138,25 +139,28 @@ def get_bytearray(encodedtext):
     # print(sum(temp))
     return bt
 
+# Have to fix this
+
 
 def encode_the_map(length,hash_map):
     val = bin(length)[2:]
     val = ((8-(len(val)%8))*"0")+val
     result = [val]
-    # print(hash_map)
     for data in hash_map:
-        x = data
-        to_add = (8-(len(x)%8))
-        zeroes = (((8-len(bin(to_add)[2:])%8))*"0")+bin(to_add)[2:]
-        result.append(zeroes)
-        result.append((to_add*"0")+x)
+        code = data
         ele = hash_map[data]
-        # print(ele)
-        y = bin(ord(ele))[2:]
-        to_add2 = 8-(len(y)%8)
-        result.append((to_add2*"0")+y)
+        zeroes_to_be_added = 8-(len(code)%8)
+        new_code = ("0"*zeroes_to_be_added)+code
+        length_bytes_to_be_read = len(new_code)//8
+        character_data = ord(ele)
+        new_code_length_bytes_to_be_read = ((8-len(bin(length_bytes_to_be_read)[2:]))*"0")+bin(length_bytes_to_be_read)[2:]
+        new_character_data = ((8-len(bin(character_data)[2:]))*"0")+bin(character_data)[2:]
+        new_zeroes_to_be_added = ((8-len(bin(zeroes_to_be_added)[2:]))*"0")+bin(zeroes_to_be_added)[2:]
+        result.append(new_zeroes_to_be_added)
+        result.append(new_code_length_bytes_to_be_read)
+        result.append(new_code)
+        result.append(new_character_data)
 
-    # print(len("".join(result)))
     return "".join(result)
 
 
@@ -184,6 +188,7 @@ def encode():
         hash_map[val] = ele
         encode_map[ele] = val
 
+    # print(hash_map)
     print("Enter the address of your output.bin file : ")
     output_file = input()
     output = open(output_file, 'wb')
@@ -191,15 +196,141 @@ def encode():
     partially_encoded = add_extra(huffman_encoded)
     map_to_be_added = encode_the_map(len(huffman_codes),hash_map)
     fully_encoded = map_to_be_added+partially_encoded
+    # print(hash_map)
+    # print(int(partially_encoded,2))
     b_array = get_bytearray(fully_encoded)
     output.write(b_array)
     return
 
 
+def make_hash_map(file_address):
+    file = open(file_address, "rb")
+    flag = 0
+    num = 1  # Garbage value
+    i = 0
+    hash_map = {}
+    byte = file.read(1)
+    while i < num and byte:
+        if flag == 0:
+            flag = 1
+            q = byte
+            int_val = int.from_bytes(q, "big")
+            v = ((8 - len(bin(int_val)[2:])) * "0") + bin(int_val)[2:]
+            num = int(v,2)
+            byte = file.read(1)
+
+        else:
+            q = byte
+            int_val = int.from_bytes(q, "big")
+            v = ((8 - len(bin(int_val)[2:])) * "0") + bin(int_val)[2:]
+            zeroes_before = int(v, 2)
+            byte = file.read(1)
+            q = byte
+            int_val = int.from_bytes(q, "big")
+            v = ((8 - len(bin(int_val)[2:])) * "0") + bin(int_val)[2:]
+            length_of_bytes = int(v,2)
+            byte = file.read(1)
+            arr = []
+            cnt = 0
+            while cnt < length_of_bytes and byte:
+                q = byte
+                int_val = int.from_bytes(q, "big")
+                v = ((8 - len(bin(int_val)[2:])) * "0") + bin(int_val)[2:]
+                arr.append(v)
+                cnt += 1
+                byte = file.read(1)
+
+            extracted_code = "".join(arr)
+            code = extracted_code[zeroes_before:]
+            q = byte
+            int_val = int.from_bytes(q, "big")
+            v = ((8 - len(bin(int_val)[2:])) * "0") + bin(int_val)[2:]
+            ascii = int(v, 2)
+            hash_map[code] = chr(ascii)
+            byte = file.read(1)
+            i = i+1
+
+    file.close()
+    return hash_map
+
+
+def file_read(file_address):
+    file = open(file_address, "rb")
+    flag = 0
+    num = 1  # Garbage value
+    i = 0
+    ans = []
+    byte = file.read(1)
+    while i < num and byte:
+        if flag == 0:
+            flag = 1
+            q = byte
+            int_val = int.from_bytes(q, "big")
+            v = ((8 - len(bin(int_val)[2:])) * "0") + bin(int_val)[2:]
+            num = int(v, 2)
+            byte = file.read(1)
+
+        else:
+            byte = file.read(1)
+            q = byte
+            int_val = int.from_bytes(q, "big")
+            v = ((8 - len(bin(int_val)[2:])) * "0") + bin(int_val)[2:]
+            length_of_bytes = int(v, 2)
+            byte = file.read(1)
+            cnt = 0
+            while cnt < length_of_bytes and byte:
+                cnt += 1
+                byte = file.read(1)
+
+            byte = file.read(1)
+            i = i + 1
+
+    while byte:
+        q = byte
+        int_val = int.from_bytes(q, "big")
+        v = ((8 - len(bin(int_val)[2:])) * "0") + bin(int_val)[2:]
+        byte = file.read(1)
+        ans.append(v)
+
+    coded_text = "".join(ans)
+    file.close()
+    return coded_text
+
+
+def get_text_array(code,hash_map):
+    ans = []
+    # g = []
+    s = ""
+    for i in code:
+        s = s + i
+        if s in hash_map:
+            ans.append(hash_map[s])
+            s = ""
+
+    # print(g)
+    return ans
+
+
 def decode():
     print("Enter the address of your decode.txt file : ")
     file_source = input()
-    decode_file = open(file_source, 'w')
+    decode_file = open(file_source, 'a')
+
+    print("Enter the address of your output.bin file : ")
+    u = input()
+    hash_map = make_hash_map(u)
+    # print(hash_map)
+    code = file_read(u)
+    result = get_text_array(code,hash_map)
+    # print("".join(result))
+    # print(hash_map)
+    # print(code)
+    ans = run_length_decode(result)
+    for i in ans:
+        decode_file.write(i)
+
     return
 
+
 encode()
+decode()
